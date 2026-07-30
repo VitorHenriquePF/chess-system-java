d37c3c941662a81463996cf6e3f1eb20a55ef92d
Vitor Henrique <vh318094@gmail.com>
2026-07-30 17:54:09 -0300

    ChessException and chessPosition

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -24,9 +24,6 @@ public class Board {
 	}
 	
 	public Piece piece(int row, int column) {
-		if (!positionExists(row, column)) {
-			throw new BoardException("Position not on the board");
-		}
 		return pieces[row][column];
 	}
 	
